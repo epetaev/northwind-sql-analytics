d83311417b94923f084b0002cb95d5cee5d1f322
Egor Petaev <petaeve42@gmail.com>
2026-07-24 23:35:31 +0300

    Update summary.sql

--- summary.sql
+++ summary.sql
@@ -21,3 +21,43 @@ SELECT
     END AS new_price
 FROM book;
 
+-- ============================================
+--Оператор WHERE
+-- ============================================
+SELECT
+    author,
+    title,
+    price,
+FROM book
+WHERE amount<10;
+
+-- ============================================
+--Операторы WHERE, OR, AND
+-- ============================================
+SELECT
+    title,
+    author,
+    price,
+    amount
+FROM book
+WHERE (price<500 OR price>600) AND price*amount>=5000; -- AND имеет большмий приоритет по сравнению с OR, поэтому при необходимости заключаем в-е в скобки
+
+
+-- ============================================
+--Операторы BETWEEN, IN
+-- ============================================
+SELECT
+    title,
+    author
+FROM book
+WHERE price BETWEEN 540.50 AND 800 AND amount = 2 OR amount = 3 OR amount = 5 OR amount = 7;
+
+-- ============================================
+--Оператор ORDER BY
+-- ============================================
+SELECT
+    author,
+    title
+FROM book
+WHERE amount BETWEEN 2 AND 14
+ORDER BY author DESC,title;

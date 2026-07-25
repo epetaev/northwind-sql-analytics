3bd36629bec8748046ce156a226993a1ee242d03
Egor Petaev <petaeve42@gmail.com>
2026-07-25 14:36:41 +0300

    Update summary.sql

--- summary.sql
+++ summary.sql
@@ -61,3 +61,48 @@ SELECT
 FROM book
 WHERE amount BETWEEN 2 AND 14
 ORDER BY author DESC,title;
+
+-- ============================================
+--Оператор WHERE (подзапрос используется два SELECT)
+-- ============================================
+SELECT author,title,price
+FROM book
+WHERE price <=(
+    SELECT AVG (price)
+    FROM book)
+    ORDER BY price DESC;
+
+-- ============================================
+--Оператор WHERE и HAVING разница на примере 
+-- ============================================
+-- Создаём тестовую таблицу
+CREATE TABLE orders (
+    order_id INT,
+    seller_id INT,
+    status TEXT,
+    gmv NUMERIC
+);
+
+-- Наполняем данными
+INSERT INTO orders VALUES
+(1, 100, 'Completed', 500),
+(2, 100, 'Completed', 300),
+(3, 100, 'Cancelled', 200),
+(4, 200, 'Completed', 100),
+(5, 200, 'Completed', 150),
+(6, 300, 'Completed', 1000),
+(7, 300, 'Completed', 1000),
+(8, 300, 'Completed', 1000);
+-- Приимер с WHERE
+SELECT seller_id, COUNT(*) AS orders_count, SUM(gmv) AS total_gmv
+FROM orders
+WHERE status = 'Completed'
+GROUP BY seller_id;
+--Результат: продавец 100 попадёт в выборку с 2 заказами (отменённый заказ #3 просто не учитывается).
+
+--Пример с HAVING — сначала группируем ВСЕ строки, а потом фильтруем уже готовые группы:
+SELECT seller_id, COUNT(*) AS orders_count, SUM(gmv) AS total_gmv
+FROM orders
+GROUP BY seller_id
+HAVING COUNT(*) >= 3;
+--Результат: останется только продавец 300 (у него 3 заказа), продавцы 100 и 200 отсеются — потому что HAVING проверяет количество заказов уже после группировки.
